96ba099937ef4550af1ddfc469f2eaa559da477e
wi <league@WTS13.attlocal.net>
2017-05-13 15:54:32 -0700

    Nest Loops & Loops

---
+++ src/ForLoopGauntlet.java
@@ -0,0 +1,26 @@
+
+public class ForLoopGauntlet {
+	public static void main(String[] args) {
+	int j=1;
+	int k=0;
+		for(int i=1;i<13;i++){
+			System.out.println(k);
+			System.out.println(j);
+			k=k+j;
+			j=k+j;
+			
+			
+			
+		}
+			}
+			
+	}
+
+	
+	
+
+	
+
+
+
+
